6ec21c23f9556970ce8f869ff501b88228fc03c6
Yahid1 <yahid.diaz@upr.udu>
2025-04-29 19:25:33 -0400

    Stryker configuration

--- Pet-Finder/filterData.js
+++
@@ -1,7 +0,0 @@
-function filterData(pets, searchQuery, selectedFilter) {
-  return pets.filter(pet =>
-    (selectedFilter === "" || pet.type.includes(selectedFilter)) &&
-    (searchQuery === "" || pet.name.includes(searchQuery))
-  );
-}
-module.exports = { filterData };
